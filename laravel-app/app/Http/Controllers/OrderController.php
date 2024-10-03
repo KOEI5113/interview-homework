08ab702205a4fd79ba97f5cc081a43575a24a112
alex.chen <alex.chen@25sprout.com>
2024-10-03 19:29:34 +0800

    - 刪除可以略過的 Exception

--- laravel-app/app/Http/Controllers/OrderController.php
+++ laravel-app/app/Http/Controllers/OrderController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Events\OrderCreateEvent;
-use App\Exceptions\OrderIdDuplicatedException;
 use App\Http\Requests\ShowOrderRequest;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Resources\OrderResource;
@@ -15,17 +14,9 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(
-        StoreOrderRequest $StoreOrderRequest,
-        OrderService $OrderService,
-    ): JsonResponse
+    public function store(StoreOrderRequest $StoreOrderRequest): JsonResponse
     {
-        $validated = $StoreOrderRequest->validated();
-        $Order = $OrderService->get($validated["id"]);
-        if (!is_null($Order)) {
-            throw OrderIdDuplicatedException::make("訂單編號已經存在");
-        }
-        OrderCreateEvent::dispatch($validated);
+        OrderCreateEvent::dispatch($StoreOrderRequest->validated());
         return response()->json();
     }
 
